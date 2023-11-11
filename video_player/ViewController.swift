188fc45432c011dd1edbf733481985867b88745b
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2023-11-11 09:28:32 -0800

    finished product!

--- video_player/ViewController.swift
+++ video_player/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,12 +16,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    private func findVideo() {
-        guard let path = Bundle.main.path(forResource: "BeActivOverview", ofType: "m4v") else {
-            debugPrint("video.m4v not found")
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        playVideo()
+    }
+    
+    private func playVideo() {
+        guard let path = Bundle.main.path(forResource: "BeActivOverview", ofType: "mp4") else {
+            debugPrint("ERROR 'BeActivOverview' NOT FOUND")
             return
         }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true) {
+            player.play()
+        }
     }
-
 }
 
+
+//private func findVideo() {
+//    guard let path = Bundle.main.path(forResource: "BeActivOverview", ofType: "m4v") else {
+//        debugPrint("video.m4v not found")
+//        return
+//    }
+//}
